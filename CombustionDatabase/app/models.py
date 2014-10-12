df4859a627b02fa4d1a3929bdda72953a403efe0
胡小强 <hu-xq11@mails.tsinghua.edu.cn>
2014-10-13 00:46:33 +0800

    添加部分model

--- CombustionDatabase/app/models.py
+++ CombustionDatabase/app/models.py
@@ -15,4 +15,29 @@ class bibligraphy(models.Model):
 	preferredKey = models.CharField(max_length = 50)
 	page = models.CharField(max_length = 20)
 
-#class bibligraphy_attatchment(models.Model):
+class bibligraphy_attatchment(models.Model):
+	primeID = models.OneToOneField(bibligraphy)
+	content = models.FileField(upload_to='document/')
+	name = models.CharField(max_length = 70)
+
+class data_attribute(models.Model):
+	primeID = models.CharField(max_length=9)
+	preferredKey = models.CharField(max_length=30)
+	origin = models.CharField(max_length=9)
+	originXML = models.TextField()
+
+class data_attribute_additionaldataiteitem(models.Model):
+	primeID = models.OneToOneField(data_attribute)
+	description = models.CharField(max_length=40)
+	content = models.TextField()
+
+class data_attribute_attatchment(models.Model):
+	primeID = models.OneToOneField(data_attribute)
+	content = models.FileField(upload_to='document/')
+	name = models.CharField(max_length=70)
+
+class data_attribute_property(models.Model):
+	primeID = models.OneToOneField(data_attribute)
+	expID = models.CharField(max_length = 9)
+	dataGroupID = models.CharField(max_length=5)
+	propertyID = models.CharField(max_length=4)
